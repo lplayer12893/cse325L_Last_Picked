171bf4d9de2fbe7753d2acfdfc86f335c2f57060
Sean Salinas <ssalinas@nmt.edu>
2015-02-11 15:32:06 -0700

    Added pseudocode to main.

--- TeamLastPicked_p3/p3.c
+++ TeamLastPicked_p3/p3.c
@@ -12,6 +12,11 @@
 
 int main(int argc, char ** argv)
 {
-	
+	// 1. Get command line arguments
+	// 2. Initialize buffer entries with -1
+	// 3. Create producer thread(s)
+	// 4. Create consumer thread(s)
+	// 5. Sleep 300 seconds
+	// 6. Exit
 	return 0;
 }
