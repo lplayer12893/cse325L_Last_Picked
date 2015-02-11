7125d7db08b5fcc60d9fe702d589d563eea50b08
Sean Salinas <ssalinas@nmt.edu>
2015-02-11 16:28:37 -0700

    Moved buffer functions into their own files.

---
+++ TeamLastPicked_p3/buffer.c
@@ -0,0 +1,125 @@
+/*
+ * filename: buffer.c
+ * description: This is an implementation of a buffer using integer arrays.
+ * authors: Salinas, Sean and Stuyvesant, Lucas
+ * class: CSE 325, Spring 2015
+ * instructor: Zheng
+ * assignment: Project #3
+ * assigned: February 11th, 2015
+ * due: February 25th, 2015
+ */
+
+#include "buffer.h"
+
+/**
+ * This function prints the state of the buffer.
+ * 
+ * @param b buffer to print
+ */
+void printBuffer(int b[])
+{
+	int i=0;
+	
+	printf("Buffer contents: \n");
+	for (i=0;i<10;i++)
+	{
+		printf("\tbuffer[%d]: %d\n",i,b[i]);
+	}
+}
+
+/**
+ * This initializes the buffer to safe values (-1)
+ * 
+ * @param b buffer to initialize
+ */
+void initializeBuffer(int b[])
+{
+	int i;
+	for (i=0;i<10;i++)
+		b[i] = -1;
+}
+
+/**
+ * This function pushes an item into the FILO buffer.
+ * 
+ * @param b buffer to use
+ * @param item item to push
+ */
+void push(int b[], int item)
+{
+	int i;
+	
+	// First, check for overflow
+	if (b[9] != -1)
+		printf("WARNING: Buffer overflow!\n");
+	
+	// Next, shift all items to the right.
+	for (i=9; i > 0; i++)
+	{
+		b[i] = b[i-1];
+	}
+	
+	// Finally, add our item
+	b[0] = item;
+}
+
+/**
+ * This function pops an item off of the FILO buffer
+ * 
+ * @param b buffer to use
+ * @return the item popped off
+ */
+int pop(int b[])
+{
+	int i;
+	
+	// First, save our return value
+	int ret = b[0];
+	
+	// Next, shift all our items left.
+	for (i=0; i<8; i++)
+	{
+		b[i] = b[i+1];
+	}
+	
+	// Next, make the last item -1
+	b[9] = -1;
+	
+	// Lastly, return our value
+	return ret;
+}
+
+/**
+ * This function enqueues an item into the FIFO buffer
+ * 
+ * @param b buffer to use
+ * @param item item to enqueue
+ */
+void enqueue(int b[], int item)
+{
+	int i;
+	
+	// First, walk the array looking for the first free spot
+	for (i=0;i<9;i++)
+	{
+		if (b[i] == -1)
+			break;
+	}
+	// Check for overflow
+	if ((i == 9) && (b[i] != -1))
+		printf("WARNING: Buffer overflow!");
+	
+	// Add our item
+	b[i] = item;
+}
+
+/**
+ * This function dequeues an item from the FIFO buffer
+ * @param b buffer to use
+ * @return item dequeued
+ */
+int dequeue(int b[])
+{
+	// Since dequeue pulls from the same side as pop, we can just call it.
+	return pop(b);	
+}
---
+++ TeamLastPicked_p3/buffer.h
@@ -0,0 +1,25 @@
+/*
+ * filename: buffer.h
+ * description: This is the header file for buffer.c, our buffer functions.
+ * authors: Salinas, Sean and Stuyvesant, Lucas
+ * class: CSE 325, Spring 2015
+ * instructor: Zheng
+ * assignment: Project #3
+ * assigned: February 11th, 2015
+ * due: February 25th, 2015
+ */
+
+#ifndef __BUFFER_H
+#define __BUFFER_H
+
+#include <stdio.h>
+
+
+void printBuffer(int b[]);
+void initializeBuffer(int b[]);
+void push(int b[], int item);
+int pop(int b[]);
+void enqueue(int b[], int item);
+int dequeue(int b[]);
+
+#endif // __BUFFER_H
