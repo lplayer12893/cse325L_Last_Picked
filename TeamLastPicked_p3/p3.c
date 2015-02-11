267e6da6e64d28c8edd806c1a354338a9040f166
Sean Salinas <ssalinas@nmt.edu>
2015-02-11 15:27:50 -0700

    Added header to p3.c

---
+++ TeamLastPicked_p3/p3.c
@@ -0,0 +1,17 @@
+/*
+ * filename: p3.c
+ * description: This is an implementation of producer/consumer model of shared
+ *  buffering, with multi-threading.
+ * authors: Salinas, Sean and Stuyvesant, Lucas
+ * class: CSE 325, Spring 2015
+ * instructor: Zheng
+ * assignment: Project #3
+ * assigned: February 11th, 2015
+ * due: February 25th, 2015
+ */
+
+int main(int argc, char ** argv)
+{
+	
+	return 0;
+}
