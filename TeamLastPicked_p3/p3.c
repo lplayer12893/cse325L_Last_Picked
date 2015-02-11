ed69d5c4b67fd3d7f2e409275529bbb17c57d7f9
Sean Salinas <ssalinas@nmt.edu>
2015-02-11 15:47:45 -0700

    Added usage to main.

--- TeamLastPicked_p3/p3.c
+++ TeamLastPicked_p3/p3.c
@@ -10,9 +10,23 @@
  * due: February 25th, 2015
  */
 
+#include <stdio.h>
+
+#define FIFO 0
+#define FILO 1
+
 int main(int argc, char ** argv)
 {
 	// 1. Get command line arguments
+	int numProducers=0,numConsumers=0,bufferMode=-1;
+	int buffer[10];
+	if (argc != 4)
+	{
+		// Wrong number of arguments
+		fprintf(stderr,"Wrong number of arguments.\nUsage: %s [producers] [consumers] [buffer mode]\n\tproducers: number of producer threads\n\tconsumers: number of consumer threads\n\tbuffer mode: the mode for the buffer\n\t\t0: FIFO\n\t\t1: FILO\n\n",argv[0]);
+		return 1;
+	}
+	
 	// 2. Initialize buffer entries with -1
 	// 3. Create producer thread(s)
 	// 4. Create consumer thread(s)
