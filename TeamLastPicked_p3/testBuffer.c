b45378ada49dc7852790edef1498487648c5d7f4
Sean Salinas <ssalinas@nmt.edu>
2015-02-11 16:51:26 -0700

    Added code to test enqueue and dequeue

--- TeamLastPicked_p3/testBuffer.c
+++ TeamLastPicked_p3/testBuffer.c
@@ -51,9 +51,37 @@ int main(void)
 	}
 	
 	// Test enqueue
+	for (i=0;i<10;i++)
+		enqueue(b,i);
+	for (i=0;i<10;i++)
+	{
+		if (b[i] != i)
+		{
+			fprintf(stderr,"b[%d]: %d (should be %d)\n",i,b[i],i);
+			printBuffer(b);
+			return 1;
+		}
+	}
 	
 	// Test dequeue
-	
+	for (i=0;i<10;i++)
+	{
+		if (dequeue(b) != i)
+		{
+			fprintf(stderr,"b[%d]: %d (should be %d)\n",i,b[i],i);
+			printBuffer(b);
+			return 1;
+		}
+	}
+	for (i=0;i<10;i++)
+	{
+		if(b[i] != -1)
+		{
+			fprintf(stderr,"b[%d]: %d (should be -1)\n",i,b[i]);
+			printBuffer(b);
+			return 1;
+		}
+	}
 	
 	
 	// Test enqueue overflow
