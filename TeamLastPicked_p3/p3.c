3021e81634171ce88f8371b1da1885383179d316
Sean Salinas <ssalinas@nmt.edu>
2015-02-11 15:56:01 -0700

    Added some string parsing and buffer initialization.

--- TeamLastPicked_p3/p3.c
+++ TeamLastPicked_p3/p3.c
@@ -19,6 +19,7 @@ int main(int argc, char ** argv)
 {
 	// 1. Get command line arguments
 	int numProducers=0,numConsumers=0,bufferMode=-1;
+	int i=0;
 	int buffer[10];
 	if (argc != 4)
 	{
@@ -26,8 +27,23 @@ int main(int argc, char ** argv)
 		fprintf(stderr,"Wrong number of arguments.\nUsage: %s [producers] [consumers] [buffer mode]\n\tproducers: number of producer threads\n\tconsumers: number of consumer threads\n\tbuffer mode: the mode for the buffer\n\t\t0: FIFO\n\t\t1: FILO\n\n",argv[0]);
 		return 1;
 	}
+	sscanf(argv[1],"%d",&numProducers);
+	sscanf(argv[2],"%d",&numConsumers);
+	sscanf(argv[3],"%d",&bufferMode);
+	
+	printf("Running with %d producers, %d consumers, and a %s buffer.\n",numProducers,numConsumers,bufferMode == FIFO? "fifo":"filo");
 	
 	// 2. Initialize buffer entries with -1
+	for (i=0;i<10;i++)
+	{
+		buffer[i] = -1;
+	}
+	
+	printf("Buffer contents: \n");
+	for (i=0;i<10;i++)
+	{
+		printf("\t%d\n",buffer[i]);
+	}
 	// 3. Create producer thread(s)
 	// 4. Create consumer thread(s)
 	// 5. Sleep 300 seconds
