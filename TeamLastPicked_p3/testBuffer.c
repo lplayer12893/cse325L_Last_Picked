cb5acc012d44fee23ba40419a52b9ad42958a35d
Sean Salinas <ssalinas@nmt.edu>
2015-02-11 16:36:20 -0700

    Added tests for buffer.

---
+++ TeamLastPicked_p3/testBuffer.c
@@ -0,0 +1,22 @@
+#include "buffer.h"
+#include <stdio.h>
+
+int main(void)
+{
+	// Test initialize
+	
+	// Test enqueue
+	
+	// Test dequeue
+	
+	// Test push
+	
+	// Test pop
+	
+	// Test enqueue overflow
+	
+	// Test dequeue overflow
+	
+	printf("All buffer tests passed.\n");
+	return 0;
+}
