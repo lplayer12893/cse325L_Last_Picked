221a4f5b2da165c9f4e957d2970a4893b6346c1a
Sean Salinas <naeslas@gmail.com>
2015-03-08 21:49:08 -0600

    Removed redundant comment, and added more definition to structures.

--- TeamLastPicked_p4/sched_impl.c
+++ TeamLastPicked_p4/sched_impl.c
@@ -46,6 +46,7 @@ static void destroy_sched_queue(sched_queue_t *queue)
 /* You need to statically initialize these structures: */
 /* This structure will have function pointers only! */
 /* The following are what we're supposed to implement:
+    // First Part
 	// Initialize a thread_info_t
 	void (*init_thread_info)    (thread_info_t *info, sched_queue_t *queue);
 
@@ -63,6 +64,21 @@ static void destroy_sched_queue(sched_queue_t *queue)
 
 	// Voluntarily relinquish the CPU when this thread's timeslice is over (cooperative multithreading).
 	void (*release_cpu)         (thread_info_t *info);
+
+	// Second Part
+	// Initialize a sched_queue_t
+	void            (*init_sched_queue)    (sched_queue_t *queue, int queue_size);
+	// Release the resources associated with a sched_queue_t
+	void            (*destroy_sched_queue) (sched_queue_t *queue);
+	// Allow a worker thread to execute.
+	void            (*wake_up_worker)      (thread_info_t *info);
+	// Block until the current worker thread relinquishes the CPU.
+	void            (*wait_for_worker)     (sched_queue_t *queue);
+	// Select the next worker thread to execute. Returns NULL if the scheduler queue is empty.
+	thread_info_t * (*next_worker)         (sched_queue_t *queue);
+	// Block until at least one worker thread is in the scheduler queue.
+	void            (*wait_for_queue)      (sched_queue_t *queue);
+
  */
 
 /* These are the functions that will be called when we are using a FIFO scheduling method */
@@ -93,21 +109,3 @@ sched_rr =
 		/*, ...etc... */
 	}
 };
-
-/* A worker thread must be able to call the following operations.
- * See scheduler.c:worker_proc() for an example of usage. */
-typedef struct worker_thread_ops {
-	/* Initialize a thread_info_t */
-	void (*init_thread_info)    (thread_info_t *info, sched_queue_t *queue);
-	/* Release the resources associated with a thread_info_t */
-	void (*destroy_thread_info) (thread_info_t *info);
-	/* Block until the thread can enter the scheduler queue. */
-	void (*enter_sched_queue)   (thread_info_t *info);
-	/* Remove the thread from the scheduler queue. */
-	void (*leave_sched_queue)   (thread_info_t *info);
-	/* While on the scheduler queue, block until thread is scheduled. */
-	void (*wait_for_cpu)        (thread_info_t *info);
-	/* Voluntarily relinquish the CPU when this thread's timeslice is
-	 * over (cooperative multithreading). */
-	void (*release_cpu)         (thread_info_t *info);
-} worker_thread_ops_t;
