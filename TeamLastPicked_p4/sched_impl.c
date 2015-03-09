ce859e22393d49267003f9a0dec7d7d0dd91c574
Sean Salinas <naeslas@gmail.com>
2015-03-08 22:10:22 -0600

    Added TODOs and comments on what every function should do.

--- TeamLastPicked_p4/sched_impl.c
+++ TeamLastPicked_p4/sched_impl.c
@@ -3,18 +3,25 @@
 
 /* Fill in your scheduler implementation code below: */
 
+/**
+ * This is a function that will init the thread info.
+ */
 static void init_thread_info(thread_info_t *info, sched_queue_t *queue)
 {
 	/*...Code goes here...*/
 }
 
+/**
+ * This is a function that will destroy a thread info.
+ */
 static void destroy_thread_info(thread_info_t *info)
 {
 	/*...Code goes here...*/
 }
 
-/*...More functions go here...*/
-
+/**
+ * This is a function that will initialize a scheduler queue.
+ */
 static void init_sched_queue(sched_queue_t *queue, int queue_size)
 {
 	if (queue == NULL)
@@ -28,6 +35,9 @@ static void init_sched_queue(sched_queue_t *queue, int queue_size)
 	queue->numInQueue = 0;
 }
 
+/**
+ * This is a function that will destroy a scheduler queue.
+ */
 static void destroy_sched_queue(sched_queue_t *queue)
 {
 	if (queue != NULL)
@@ -41,54 +51,98 @@ static void destroy_sched_queue(sched_queue_t *queue)
 
 }
 
+/**
+ * This is a function that will put a thread into the scheduler queue.
+ */
 static void enter_sched_queue(thread_info_t *info)
 {
-
+	// TODO: Write this function out.
+	// Check if the queue has room
+	// If it does, add to the queue.
+	// If it doesn't, block until we can
 }
 
+/**
+ * This will be called when the worker has completed.
+ */
 static void leave_sched_queue(thread_info_t *info)
 {
-
+	// TODO: Write this function out.
+	// Remove from the queue
+	// Update the queue stats
+	// Maybe wake up something that is waiting?
 }
 
+/**
+ * This is a function that will block until the scheduler allows it to run.
+ */
 static void fifo_wait_for_cpu(thread_info_t * info)
 {
-
+	// TODO: Write this function out.
+	// Check if the CPU is available.
+	// If it is available, return immediately.
+	// If it isn't, then block until it is.
 }
 
+/**
+ * This will be called when the thread is ready to release the CPU
+ */
 static void fifo_release_cpu(thread_info_t * info)
 {
-
+	// TODO: Write this function out.
+	// Since we are FIFO, I think we just return immediately, and let the process continue.
 }
 
+/**
+ * This will be called when the thread is ready to pause and wait its next turn.
+ */
 static void rr_release_cpu(thread_info_t * info)
 {
-
+	// TODO: Write this function out.
+	// Put this in the queue, at the end.
 }
 
+/**
+ * This should block until its our turn in the queue.
+ */
 static void rr_wait_for_cpu(thread_info_t * info)
 {
-
+	// TODO: Write this function out.
+	// Block until we are up in the queue.
 }
 
+/**
+ * This is called by the scheduler.
+ */
 static void wake_up_worker (thread_info_t *info)
 {
-
+	// TODO: Write this function out.
 }
+
+/**
+ * This is called by the scheduler.
+ */
 static void wait_for_worker(sched_queue_t *queue)
 {
-
+	// TODO: Write this function out.
 }
 
+/**
+ * This is called by the scheduler.
+ */
 thread_info_t * next_worker(sched_queue_t *queue)
 {
+	// TODO: Write this function out.
 	return NULL;
 }
+
+/**
+ * This is called by the scheduler.
+ */
 static void wait_for_queue (sched_queue_t *queue)
 {
-
+	// TODO: Write this function out.
 }
-/*...More functions go here...*/
 
 /* You need to statically initialize these structures: */
 /* This structure will have function pointers only! */
