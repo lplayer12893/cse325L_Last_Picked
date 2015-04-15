5446f0f7bd179185db1d60ded808a0c11f625195
Sean Salinas <ssalinas@nmt.edu>
2015-04-15 16:00:11 -0600

    Added data structure to rbtree.h

--- TeamLastPicked_p6/rbtree.h
+++ TeamLastPicked_p6/rbtree.h
@@ -4,4 +4,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void * getElement(int id);
+void insertElement(void * payload, int id);
+void deleteElement(int id);
+
+typedef struct _node node;
+struct _node
+{
+	int id;
+	node * parent;
+	node *left;
+	node *right;
+	void * payload;
+};
+
 #endif // __RBTREE_H
