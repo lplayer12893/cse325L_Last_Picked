7a53ec71144ccbe757b3a34aea6d26e6203da038
Sean Salinas <ssalinas@nmt.edu>
2015-04-15 15:48:24 -0600

    Added testdisk.c for our testing purposes.

---
+++ TeamLastPicked_p6/testDisk.c
@@ -0,0 +1,9 @@
+#include "disk.h"
+
+int main(int argc, char ** argv)
+{
+	// This is where we add our own tests. Make will automatically run these. 
+	// To error out, just return any non-zero integer.
+	
+	return 0;
+}
