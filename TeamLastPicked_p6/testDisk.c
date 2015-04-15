098d521233a0a6514ef8b95712a6338f07d4f8b0
Sean Salinas <ssalinas@nmt.edu>
2015-04-15 15:53:54 -0600

    Added comment to testdisk.

--- TeamLastPicked_p6/testDisk.c
+++ TeamLastPicked_p6/testDisk.c
@@ -4,6 +4,8 @@ int main(int argc, char ** argv)
 {
 	// This is where we add our own tests. Make will automatically run these. 
 	// To error out, just return any non-zero integer.
+
+	// First, let's test our red black tree
 	
 	return 0;
 }
