b85d221da5913535cf1b13a0bcd2d37eab07bae2
Sean Salinas <naeslas@gmail.com>
2015-05-03 19:15:06 -0600

    Added data structure.

--- TeamLastPicked_p6/myfilesys.h
+++ TeamLastPicked_p6/myfilesys.h
@@ -2,19 +2,30 @@
 #define _MYFILESYS_H_
 
 /********************************************************************************************************/
-int make_fs(char *disk_name);   /* create an empty file system on the virtual disk with name disk_name  */
-int mount_fs(char *disk_name);  /* mounts a file system stored on a virtual disk with name disk_name    */
-int unmount_fs(char *disk_name);    /* unmounts the file system from a virtual disk with name disk_name */
-int fs_open(char *name);    /* the file specified by name is opened for reading and writing, and the    */
-                                /* file descriptor is returned                                          */
-int fs_close(int fildes);   /* closes the file with the corresponding file descriptor fildes            */
-int fs_create(char *name);  /* creates a new file with name name                                        */
-int fs_delete(char *name);  /* deletes the file with name name                                          */
-int fs_read(int fildes, void *buf, size_t nbyte);   /* Attempts to read nbyte bytes from a given file   */
-int fs_write(int fildes, void *buf, size_t nbyte);  /* Attempts to write nbyte bytes from a given file  */
-int fs_get_filesize(int fildes);    /* returns the current size of a given file                         */
+int make_fs(char *disk_name); /* create an empty file system on the virtual disk with name disk_name  */
+int mount_fs(char *disk_name); /* mounts a file system stored on a virtual disk with name disk_name    */
+int unmount_fs(char *disk_name); /* unmounts the file system from a virtual disk with name disk_name */
+int fs_open(char *name); /* the file specified by name is opened for reading and writing, and the    */
+/* file descriptor is returned                                          */
+int fs_close(int fildes); /* closes the file with the corresponding file descriptor fildes            */
+int fs_create(char *name); /* creates a new file with name name                                        */
+int fs_delete(char *name); /* deletes the file with name name                                          */
+int fs_read(int fildes, void *buf, size_t nbyte); /* Attempts to read nbyte bytes from a given file   */
+int fs_write(int fildes, void *buf, size_t nbyte); /* Attempts to write nbyte bytes from a given file  */
+int fs_get_filesize(int fildes); /* returns the current size of a given file                         */
 int fs_lseek(int fildes, off_t offset); /* sets the file pointer to the argument offset                 */
-int fs_truncate(int fildes, off_t length);  /* truncates a given file to length bytes in size           */
+int fs_truncate(int fildes, off_t length); /* truncates a given file to length bytes in size           */
 /********************************************************************************************************/
+
+struct fs_meta
+{
+	struct fs_meta *next; // Pointer to the next metadata
+	struct fs_meta *prev; // Pointer to the previous metadata
+	char *file_name; // String of file name, NULL if not in use
+	int total_bytes; // Number of bytes used
+	void * data; // Pointer to the actual data, the bucket
+	int bytes_here; // Number of bytes in this bucket
+	struct fs_meta * frag_next; // Pointer to the next metadata if fragmented, NULL if not fragmented.
+};
 
 #endif
